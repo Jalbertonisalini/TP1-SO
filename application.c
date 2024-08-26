639548adc8918b36b39783951f0247704854b130
cachoJDG <jgago@itba.edu.ar>
2024-08-26 13:17:51 -0300

    fix tuberia

--- application.c
+++ application.c
@@ -1,5 +1,3 @@
-//application.c
-
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -8,68 +6,63 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-
 #define SLAVES 2
 
-int main(int argc, char * argv[]){
+int main(int argc, char *argv[]) {
 
-    char * PATH = "./child";
-    char * text = "rojo";
+    char *PATH = "./child";
+    char *text = "rojo";
     int status;
 
     int pipefd[2];
     pid_t pidChild;
 
-    if(pipe(pipefd) == -1){
+    if (pipe(pipefd) == -1) {
         perror("Error while creating pipe");
-    }
-    printf("%d \n", pipefd[0]);
-    printf("%d \n", pipefd[1]);
-
-//    close(STDIN_FILENO);
-    //close(1);
-    
-    dup2(pipefd[0], STDIN_FILENO);
-    //dup(4);
-
-    if((pidChild = fork()) < 0){
-        perror("Error while creating child");
-        exit(1);
-    }
-    else if (pidChild == 0){ /* child process */
-        char * args[] = {PATH, text, NULL};
-        char *envp[] = {NULL};
-        execve(PATH,args,envp);
+        exit(EXIT_FAILURE);
     }
 
-    close(0);
-    close(1);
-    open("/dev/tty", O_RDWR);
-    open("/dev/tty", O_RDWR);
+    printf("Pipe read descriptor: %d \n", pipefd[0]);
+    printf("Pipe write descriptor: %d \n", pipefd[1]);
 
-    write(pipefd[1],"Hola chuan\n", 11);
+    // Redirigir la entrada estándar al descriptor de lectura de la tubería
+    close(STDIN_FILENO); // Cierra la entrada estándar
+    dup2(pipefd[0], STDIN_FILENO); // Redirige la entrada estándar al descriptor de lectura
+    close(pipefd[0]); // Cierra el descriptor original de lectura
+    close(pipefd[1]); // Cierra el descriptor de escritura
 
+    // Crear SLAVES cantidad de procesos hijos
+    for (int i = 0; i < SLAVES; i++) {
+        
+        if ((pidChild = fork()) < 0) {
+            perror("Error while creating child");
+            exit(EXIT_FAILURE);
+        
+        } else if (pidChild == 0) { /* child process */
+            char *args[] = {PATH, text, NULL};
+            char *envp[] = {NULL};
+            execve(PATH, args, envp);
+            perror("execve failed"); // Manejo de error si execve falla
+            exit(EXIT_FAILURE);
+        }
+    }
 
-//    for (int i = 0; i < SLAVES; i++){
-//
-//        if((pidChild = fork()) < 0){
-//            perror("Error while creating child");
-//            exit(1);
-//        }
-//        else if (pidChild == 0){ /* child process */
-//            char * args[] = {PATH, text, NULL};
-//            char *envp[] = {NULL};
-//            execve(PATH,args,envp);
-//        }
-//    }
-//    int pid2;
-//    //sleep(12);
-//    for(int i = 0; i < SLAVES; i++) {
-//        pid2 = waitpid(-1, &status, 0);
-//        printf("%d \n", pid2);
-//    }
-    int pid2 = waitpid(-1, &status, 0);
-    printf("Todos han terminado");
+    // En el proceso padre, manejar la tubería
+    close(pipefd[0]); // El proceso padre no lee de la tubería
+    write(pipefd[1], "Hola chuan\n", 11); // Escribe en la tubería
+    close(pipefd[1]); // Cierra el descriptor de escritura
+
+    // Esperar a que todos los procesos hijos terminen
+    for (int i = 0; i < SLAVES; i++) {
+        int pid = waitpid(-1, &status, 0);
+        if (pid == -1) {
+            perror("waitpid failed");
+            exit(EXIT_FAILURE);
+        }
+        printf("Child process %d finished with status %d\n", pid, WEXITSTATUS(status));
+    }
+
+    printf("Todos han terminado\n");
+
     return 0;
 }
-
