2aa3601e68d118e95db48d42dab56dab2d1a3c52
Juan Bautista Albertoni Salini <juanbaalbertoni@gmail.com>
2024-08-26 12:59:40 -0300

    Updates
    Co-authored-by: Juan_Oliva <jolivamorroni@itba.edu.ar>
    Co-authored-by: Juan_Gago <jgago@itba.edu.ar>

--- application.c
+++ application.c
@@ -1,38 +1,75 @@
+//application.c
+
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+#define SLAVES 2
 
 int main(int argc, char * argv[]){
 
     char * PATH = "./child";
-    char * args[] = {PATH, NULL};
+    char * text = "rojo";
     int status;
 
-    int pid = fork();
+    int pipefd[2];
+    pid_t pidChild;
 
+    if(pipe(pipefd) == -1){
+        perror("Error while creating pipe");
+    }
+    printf("%d \n", pipefd[0]);
+    printf("%d \n", pipefd[1]);
 
-    if(pid == -1){
+//    close(STDIN_FILENO);
+    //close(1);
+    
+    dup2(pipefd[0], STDIN_FILENO);
+    //dup(4);
+
+    if((pidChild = fork()) < 0){
         perror("Error while creating child");
         exit(1);
     }
-    else if (pid == 0){ /* child process */
-        execve(PATH,args,NULL);
-    }
-    int pid2;
-    sleep(12);
-    pid2 = waitpid(-1, &status, 0);
-    if(pid == pid2)
-    {
-        printf("Yupi \n");
-    }
-    else
-    {
-        printf("noYupi, it did not ended \n");
-        printf("%i",status);
+    else if (pidChild == 0){ /* child process */
+        char * args[] = {PATH, text, NULL};
+        char *envp[] = {NULL};
+        execve(PATH,args,envp);
     }
 
+    close(0);
+    close(1);
+    open("/dev/tty", O_RDWR);
+    open("/dev/tty", O_RDWR);
+
+    write(pipefd[1],"Hola chuan\n", 11);
+
+
+//    for (int i = 0; i < SLAVES; i++){
+//
+//        if((pidChild = fork()) < 0){
+//            perror("Error while creating child");
+//            exit(1);
+//        }
+//        else if (pidChild == 0){ /* child process */
+//            char * args[] = {PATH, text, NULL};
+//            char *envp[] = {NULL};
+//            execve(PATH,args,envp);
+//        }
+//    }
+//    int pid2;
+//    //sleep(12);
+//    for(int i = 0; i < SLAVES; i++) {
+//        pid2 = waitpid(-1, &status, 0);
+//        printf("%d \n", pid2);
+//    }
+    int pid2 = waitpid(-1, &status, 0);
     printf("Todos han terminado");
     return 0;
 }
+
--- child.c
+++ child.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(){
-    sleep(10);
-    printf("Hola a todos");
+int main(int argc, char * argv[]){
+
+    char buffer[256];
+    printf("Hola a todos \n");
+    printf("lo que mande es %s \n", argv[1]);
+    read(STDIN_FILENO, buffer, 11);
+    printf("Message got from pipe %s \n", buffer);
     return 0;
 }
